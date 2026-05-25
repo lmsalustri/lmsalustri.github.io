1ccc7c271ee1d83f8a3404353a60bd017b55d053
Laurie Salustri <150742114+lmsalustri@users.noreply.github.com>
2026-05-24 20:43:12 -0400

    Update +page.svelte

--- src/routes/wpi-ndsa/+page.svelte
+++ src/routes/wpi-ndsa/+page.svelte
@@ -1,1 +1,46 @@
+<svelte:head>
+	<title>Home | WPI Neurodivergent Student Association</title>
+	<meta
+		name="description"
+		content="WPI's Neurodivergent Student Association supports students with autism, ADHD, dyslexia, OCD, and other neurodiverse identities through events, community, and resources."
+	/>
+</svelte:head>
 
+<main>
+	<section class="welcome-section">
+		<h1 class="welcome">Welcome!</h1>
+		<p class="tagline">
+			We are here to help support your experience at WPI through bonding activities,
+			support on navigating neurodiverse issues, and fun social events.
+		</p>
+		<p class="subtagline">No formal diagnosis required.</p>
+		<p class="button">
+			<a
+				href="https://mywpi.wpi.edu/NDSA/login_only?redirect=%2fNDSA%2fclub_signup"
+				target="_blank"
+				rel="noopener"
+			>
+				Join Us!
+			</a>
+		</p>
+	</section>
+
+	<section class="event-blog-container">
+		<div class="event-box">
+			<h2>Next Events</h2>
+			<h3>Fall 2025 General Body Meetings</h3>
+			<p>Mondays, 5-7 PM, Stratton Hall 301</p>
+		</div>
+
+		<div class="blog-box">
+			<h2>Latest Post</h2>
+			<h3>Special Interest Spotlight: Classic Rock</h3>
+			<p class="author">by Laurie Salustri | October 9, 2024</p>
+			<p>
+				Classic rock is a special interest of mine that has remained constant throughout
+				my entire life. My parents grew up listening to it, and we always have 80s on 8
+				or Classic Rewind playing on the radio.
+			</p>
+		</div>
+	</section>
+</main>
