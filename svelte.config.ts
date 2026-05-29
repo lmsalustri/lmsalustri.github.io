229846a42fc98fb2376641e4688e7dd4860ea8da
Laurie Salustri <150742114+lmsalustri@users.noreply.github.com>
2026-05-28 20:10:46 -0400

    Update and rename svelte.config.js to svelte.config.ts

--- svelte.config.ts
+++ svelte.config.ts
@@ -1,7 +1,8 @@
 import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+import type { Config } from '@sveltejs/kit';
 
-const config = {
+const config: Config = {
   preprocess: vitePreprocess(),
 
   kit: {
